bd1a0e897c7d4cd579a5757e58c0a29eac0636c3
jojohappy <jojohappy@126.com>
2013-07-01 12:45:30 +0800

    demo finish

--- controllers/app.rb
+++ controllers/app.rb
@@ -2,22 +2,25 @@ require 'json'
 
 def db
   @@db ||=
-    db = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
-      EM::Mongo::Connection.new('172.16.0.9', 27017, 1, {:reconnect_in => 1}).db('test')
-    end
+  db = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
+    #newdb = EM::Mongo::Connection.new('172.16.0.9', 27017, 1, {:reconnect_in => 1}).db('test')
+    #newdb = EM::Mongo::Connection.new('172.17.10.137', 27017, 1, {:reconnect_in => 1}).db('admin')
+    newdb = EM::Mongo::Connection.new('172.17.10.215', 27017, 1, {:reconnect_in => 1}).db('admin')
+    newdb.authenticate('root', '123456')
+    newdb
+  end
   return @@db
 end
 
 get '/' do
-	listarray = []
-	db.collection("users").find.each do |doc|
-	  if doc then
-	  aa = doc['_id'].to_s
-	  #listarray.push(aa)
-	  end
-	end
-	#db.collection("users").insert({"item" => "user111", "py" => 13})
-	content_type :json
-	puts listarray.count()
-	listarray.to_json
+  listarray = []
+  #db.collection("users").insert({"item" => "user11166", "py" => 16})
+  content_type :json
+  coll = db.collection("users")
+
+  rep = EM::Synchrony.sync coll.find.defer_as_a
+  listarray = rep.each do |doc|
+    doc
+  end
+  listarray.to_json
 end
